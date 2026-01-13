0c3c92dd7d61f8c81b98c154099369e7f2f4a623
lokman.hossen <lokman.hossen@upaybd.com.bd>
2026-01-13 16:26:49 +0600

    uber jar

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "36fc785e-e2ce-41af-97b0-f23abdf39e67";
+private static final String ID = "5f7b82e5-e48d-4a71-9e76-48cb798929af";
 }
