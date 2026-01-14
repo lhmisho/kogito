e61c08215d3e6ec01852f83102078442a100b94e
lokman.hossen <lokman.hossen@upaybd.com.bd>
2026-01-14 11:14:18 +0600

    latest code

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "63ee8a9b-7043-4dfb-932d-2c29fddaf4f8";
+private static final String ID = "3c9a5cf9-5ab5-494b-ad5e-3c6835cf8d82";
 }
