5e8f0e63520c824d600c7618a2dcc562935110c7
lokman.hossen <lokman.hossen@upaybd.com.bd>
2026-05-11 12:15:28 +0600

    risk type added with aml txn

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "00a9981a-06b4-4517-97ed-5d8e392c7e7a";
+private static final String ID = "cf738675-c08d-4a84-9e77-45c00eb5e890";
 }
